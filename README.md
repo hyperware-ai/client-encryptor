7bfed99f78de3c017c7ce02ebff4c1f989ede2a3
hosted-fornet <hosted-fornet@protonmail.com>
2025-02-17 10:39:14 -0800

    rename kinode -> hyperware & clean up code

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
 # client-api
 
-A library for handling encryption and decryption of WebSocket data (and later HTTP) from a Kinode.
+A library for handling encryption and decryption of WebSocket data (and later HTTP) for Hyperware.
 
 ## Installation
 
-`npm install @kinode/client-api`
+```
+npm install @hyperware-ai/client-api
+```
 or
-`yarn add @kinode/client-api`
+```
+yarn add @hyperware-ai/client-api
+```
 
 ## How to use
 
-Import and instantiate the KinodeClientApi:
-
+Import and instantiate the HyperwareClientApi:
 
 ```
-import KinodeClientApi from '@kinode/client-api'
-const api = new KinodeClientApi({
+import HyperwareClientApi from '@hyperware-ai/client-api'
+const api = new HyperwareClientApi({
   nodeId, // You should set this on the window and then pass in. See the apps_home html for an example.
   channelId, // The channel that WS messages will come over, usually the process name or ID.
   uri = 'ws://' + window.location.host,
   onMessage = () => null, // Handle WS message
-  onEncryptionReady = () => null, // Called when end-to-end encryption with the Kinode is ready. Recommended to use this instead of onOpen
+  onEncryptionReady = () => null, // Called when end-to-end encryption with Hyperware is ready. Recommended to use this instead of onOpen
   onOpen = () => null, // Handle WS open
   onClose = () => null, // Handle WS close
   onError = () => null, // Handle WS error
--- index.ts
+++ index.ts
@@ -72,7 +72,6 @@ export default class UqbarEncryptorApi {
     onOpen = () => null,
     onClose = () => null,
     onError = () => null,
-    // onEncryptionReady = () => null,
   }: {
     nodeId: string,
     processId: string,
@@ -82,7 +81,6 @@ export default class UqbarEncryptorApi {
     onOpen?: (ev: Event, api: UqbarEncryptorApi) => void,
     onClose?: (ev: CloseEvent) => void,
     onError?: (ev: Event) => void,
-    // onEncryptionReady?: (api: UqbarEncryptorApi) => void,
   }) {
     this._secret = undefined;
     this.processId = processFromString(processId);
@@ -91,152 +89,27 @@ export default class UqbarEncryptorApi {
     this._ws = new WebSocket(uri)
     this._ws.onmessage = async (ev: MessageEvent<string | Blob>) => { // eslint-disable-line
       onMessage(ev.data, this)
-      // figure out if it's encrypted, if it is, decrypt it and then pass to onMessage
-      // if (typeof ev.data === 'string') {
-      //   onMessage(ev.data, this)
-      // } else if (ev.data instanceof Blob) {
-      //   const encrypted = await blobToUint8Array(ev.data)
-      //   const decrypted: any = this._decrypt(encrypted) // eslint-disable-line
-      //   if (decrypted === null) {
-      //     console.log('Unable to decrypt message, passing through as-is')
-      //     const string = new TextDecoder().decode(encrypted);
-      //     onMessage(string, this)
-      //   } else {
-      //     onMessage(decrypted, this)
-      //   }
-      // } else {
-      //   onMessage(ev.data, this)
-      // }
     }
     this._ws.onopen = (ev: Event) => {
-      // Register this API instance with the server
-      // this._ws.send(stringifyAndEncode({
-      //     auth_token: getCookie(`kinode-auth_${nodeId}`),
-      //     encrypted: false,
-      // }))
       onOpen(ev, this)
     }
     this._ws.onclose = onClose
     this._ws.onerror = onError
-
-    // const rsa = forge.pki.rsa; // eslint-disable-line
-    // const keypair = rsa.generateKeyPair({bits: 2048, e: 65537}); // eslint-disable-line
-
-    // const publicKeyHex = keypair.publicKey.n.toString(16) // eslint-disable-line
-
-    // fetch(genFetchRoute('/encryptor:sys:kinode'), {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     channel_id: channelId,
-    //     public_key_hex: publicKeyHex,
-    //   })
-    // }).then(r => r.json()).then((json: { encrypted_secret: string, signed_public_key: string }) => {
-    //   const { encrypted_secret, signed_public_key } = json
-    //   fetch(`/qns_indexer:qns_indexer:kinode/node/${this.nodeId}`).then(r => r.json()).then((pqi: any) => { // eslint-disable-line
-    //     const { public_key } = pqi
-    //     console.log(this.nodeId)
-    //     console.log('Got public key from QNS:', public_key)
-    //     // get the public key from the json
-    //     const networkingPublicKey = public_key.replace('0x', '')
-
-    //     if (!networkingPublicKey) return
-
-    //     const verified: boolean = forge.pki.ed25519.verify({
-    //       message: Buffer.from(publicKeyHex, 'hex'),
-    //       signature: Buffer.from(signed_public_key, 'hex'),
-    //       publicKey: Buffer.from(networkingPublicKey, 'hex'),
-    //     })
-
-    //     console.log('Verified:', verified)
-
-    //     if (true || verified) {
-    //       const encryptedBytes = forge.util.hexToBytes(encrypted_secret)
-    //       // dc72a3afeb53d5c84c1143d179b041ccf121aff3854ec14c27080ea3eba801d1 - the secret key we expect to see
-    //       const secretKey = keypair.privateKey.decrypt(encryptedBytes, "RSA-OAEP", {
-    //         md: forge.md.sha256.create()
-    //       }) // eslint-disable-line
-
-    //       this._secret = forge.util.bytesToHex(secretKey)
-    //       this._cipher = forge.cipher.createCipher('AES-GCM', secretKey);
-    //       this._decipher = forge.cipher.createDecipher('AES-GCM', secretKey);
-    //       onEncryptionReady(this)
-    //     } else {
-    //       console.error('Unable to verify networking key')
-    //     }
-
-    //   }).catch(console.error)
-    // }).catch(console.error)
   }
 
   // methods
   _encrypt = (message: string) => {
-  //   if (this._cipher) {
-  //     const nonce = forge.random.getBytesSync(12);
-
-  //     this._cipher.start({ iv: nonce });
-  //     this._cipher.update(forge.util.createBuffer(message, 'utf8'));
-  //     this._cipher.finish();
-
-  //     const encryptedData = this._cipher.output.getBytes();
-  //     const tag = this._cipher.mode.tag.getBytes();
-
-  //     return { encrypted: encryptedData + tag, nonce }
-  //   }
-
     return null
   }
   _decrypt = (dataAndNonce: Uint8Array): any => { // eslint-disable-line
-  //   if (!this._decipher) return null
-
-  //   const encryptedBytes = dataAndNonce.slice(0, -28)
-  //   const tagBytes = dataAndNonce.slice(-28, -12)
-  //   const nonceBytes = dataAndNonce.slice(-12)
-
-  //   this._decipher.start({
-  //     iv: forge.util.createBuffer(nonceBytes),
-  //     tag: forge.util.createBuffer(tagBytes),
-  //   })
-  //   this._decipher.update(forge.util.createBuffer(encryptedBytes))
-  //   const pass = this._decipher.finish()
-
-  //   if (!pass) return null
-
-  //   return this._decipher.output.toString() as string // eslint-disable-line
   }
   send = ({ data }: SendParams) => {
-    const auth_token = getCookie(`kinode-auth_${this.nodeId}`) // eslint-disable-line
-    const ws_auth_token = getCookie(`kinode-ws-auth_${this.nodeId}`) // eslint-disable-line
+    const auth_token = getCookie(`hyperware-auth_${this.nodeId}`) // eslint-disable-line
+    const ws_auth_token = getCookie(`hyperware-ws-auth_${this.nodeId}`) // eslint-disable-line
 
-    // if (encrypted) {
-    //   if (!this._cipher) return console.error('No cipher, unable to encrypt')
-    //   this._ws.send(stringifyAndEncode({
-    //     EncryptedWsMessage: {
-    //       auth_token,
-    //       ws_auth_token,
-    //       channel_id: channelId,
-    //       target,
-    //       ...this._encrypt(JSON.stringify(data))
-    //     }
-    //   }))
-    // } else {
       this._ws.send(stringifyAndEncode(
         data
-      // {
-      //   channel_id: this.channelId,
-      //   operation: data
-      // }
-        // WsMessage: {
-        //   auth_token,
-        //   ws_auth_token,
-        //   channel_id: channelId,
-        //   target: {
-        //     node: target.node,
-        //     process: typeof target.process === 'number' ? { Id: target.process } : { Name: target.process }
-        //   },
-        //   json: data, // eslint-disable-line
-        // }
       ))
-    // }
   }
   fetchJson = async <T>(input: RequestInfo | URL, init?: RequestInit | undefined): Promise<T> => {
     console.log('Fetching JSON:', input)
